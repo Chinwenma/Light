733ddfbfaef4c62480b4f0d02559784d830c8fe1
Chinwenm Okorie <okoriechinwe1327@gmail.com>
2025-04-15 04:40:40 +0100

    hero setup

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -6,15 +6,15 @@ import Link from "next/link";
 
 export const Navbar = () => {
   return (
-    <header className="sticky top-0">
-      <div className="flex justify-center items-center py-3 bg-black text-white text-sm">
+    <header className="sticky top-0 backdrop-blur-sm z-20 ">
+      <div className="flex justify-center items-center py-3 bg-black text-white text-sm gap-3">
         <div className="inline-flex gap-1 items-center justify-center">
           <p>Get Starded for free</p>
-          <p className="text-white/60">streamline your work flow</p>
+          <p className="text-white/60 hidden md:block ">streamline your work flow</p>
           <ArrowRight className="h-4 w-4 inline-flex justify-center items-center" />
         </div>
       </div>
-      <div className="py-5">
+      <div className="py-2">
         <div className="container">
           <div className="flex items-center justify-between">
             <Image src={Logo} alt="logo" height={40} width={40} />
